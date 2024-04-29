e2a2f45647ab8ed35a54908c1ea132e78aff0314
ccelinaw <164883843+ccelinaw@users.noreply.github.com>
2024-04-28 23:47:08 -0400

    Create music.py

---
+++ music.py
@@ -0,0 +1,26 @@
+class Music:
+  def __init__(self, artist_name, track_name, streams, danceability, energy, liveness):
+    self.__artist_name = artist_name
+    self.__track_name = track_name
+    self.__streams = streams
+    self.__danceability = danceability
+    self.__energy = energy
+    self.__liveness = liveness
+
+  def get_artist_name(self):
+    return self.__artist_name
+
+  def get_track_name(self):
+    return self.__track_name
+
+  def get_streams(self):
+    return self.__streams
+
+  def get_danceability(self):
+    return self.__danceability
+
+  def get_energy(self):
+    return self.__energy
+
+  def get_liveness(self):
+    return self.__liveness
