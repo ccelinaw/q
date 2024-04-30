c6855263b7380f0eedf3937ed15416dd6f12b35b
ccelinaw <164883843+ccelinaw@users.noreply.github.com>
2024-04-29 23:43:41 -0400

    Update song.py

--- song.py
+++ song.py
@@ -1,99 +1,63 @@
-class Music:
+from music import Music
+
+class Song(Music):
   '''
-  A parent music object that has the artist's name, a list of their songs, and their metrics (danceability, energy, liveness)
+  A song (child) object that inherits attributes and methods from the Music class. Contains all the attributes and methods of a song.
 
-Attributes
-----------
-artist_name: string
-  The name of the artist of the song.
-streams: integer
-  The amount of streams the song has.
-track_name: string
-  The name of the song.
-danceability: integer
-  How danceable a song is (the user's tendency to dance to this song)
-energy: integer
-  The energy level of a song (high, low)
-liveness = integer
-  The presence of live performance elements (audience, cheering)
+  Attributes
+  ----------
+  year_released: integer
+    The year the song was released.
+    
+  Methods
+  -------
+  get_year_released -> integer
+    Returns the year the song was released.
+  average_danceability -> string
+    Returns the average danceability of a song, depending on the user's inputted danceability and the dataset's danceability value
 
-Methods
--------
-get_artist_name -> string
-  Returns the name of the artist
-get_streams -> integer
-  Returns the number of streams a song has
-get_track_name -> string
-  Returns the track name of the song
-get_danceability -> integer
-  Returns the danceability of a song
-get_energy -> integer
-  Returns the energy level of a song
-get_liveness -> integer
-  Returns the liveness of a song
-set_streams -> None
-  Sets the number of streams a song has
-set_track_name -> None
-  Sets the track name of the song
-set_artist_name -> None
-  Sets the name of the artist
-set_danceability -> None
-  Sets the danceability of a song
-add_streams -> string
-  Updates the song's streams depending on how many times the user streamed it
-'''
-  def __init__(self, artist_name, track_name, streams, danceability, energy, liveness):
-    self.__artist_name = artist_name
-    self.__track_name = track_name
-    self.__streams = streams
-    self.__danceability = danceability
-    self.__energy = energy
-    self.__liveness = liveness
-
-  def get_artist_name(self):
-    return self.__artist_name
-
-  def get_track_name(self):
-    return self.__track_name
-
-  def get_streams(self):
-    return self.__streams
-
-  def get_danceability(self):
-    return self.__danceability
-
-  def get_energy(self):
-    return self.__energy
-
-  def get_liveness(self):
-    return self.__liveness
-
-  def set_streams(self, streams):
-    self.__streams = streams
-
-  def set_track_name(self, track_name):
-    self.__track_name = track_name
-
-  def set_artist_name(self, artist_name):
-    self.__artist_name = artist_name
-
-  def set_danceability(self, danceability):
-    self.__danceability = danceability
-
-  # Method to update streams based on a multiplier factor
-  def add_streams(self, additional_streams):
+  '''
+  def __init__(self, artist_name, streams, track_name, danceability, energy, liveness, year_released):
     '''
-    Updates the stream count of a song after the user has listened to it
+    Constructor to build a song object
 
     Parameters
     ----------
-    additional_streams
-      The value to increase the number of streams by
+    artist_name
+      The name of the song's artist
+    streams
+      The number of streams the song has
+    track_name
+      The name of the song
+    danceability
+      How danceable a song is
+    energy
+      How energetic the song is
+    liveness
+      The presence of live performance elements
+    year_released
+      The year the song was released
+    '''
+    super().__init__(artist_name, track_name, streams, danceability, energy, liveness)
+    self.__year_released = year_released
+
+  # Method to get the year released of a song
+  def get_year_released(self):
+    return self.__year_released
+
+  def average_danceability(self, new_danceability):
+    '''
+    Updates the danceability of a song based on the user's tendency to dance to it. The user's input will be averaged with the dataset's danceability value to achieve the new danceability value.
+
+    Parameters
+    ----------
+    new_danceability
+      The danceability ranking that the user has given the song.
 
     Returns
     -------
     string
-      Returns a string that states the song's name, the artist's name and the new number of streams
+      Returns a string that states the track name, artist's name and the new danceability value
     '''
-    self.__streams += additional_streams
-    return f'Updated stream count for "{self.__track_name}" by {self.__artist_name}: {self.__streams}'
+    self._set_danceability = ((self.get_danceability() + new_danceability) / 2)
+    return f'Updated danceability for "{self.get_track_name()}" by {self.get_artist_name()}: {self.get_danceability()}'
